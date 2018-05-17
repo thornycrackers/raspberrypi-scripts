9d3829b773650155ca316264cb23d21f66b5b05f
Cody Hiar <codyfh@gmail.com>
2018-05-16 20:30:23 -0600

    Cleaning up code, changing lcd pin

--- lcd.py
+++ lcd.py
@@ -18,7 +18,7 @@ lcd_rs = 25
 lcd_en = 24
 lcd_d4 = 23
 lcd_d5 = 17
-lcd_d6 = 21
+lcd_d6 = 18
 lcd_d7 = 22
 
 
--- temp.py
+++ temp.py
@@ -1,4 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Python script to read temperature.
+
+I plug the sensor data into pin 4 on the raspberry pi which requires pin 4 to
+be a gpio pin. If the modprobe commands don't results in the device showing up
+in the bus folder then make sure the following line is in `/boot/config.txt`
+
+```
+dtoverlay=w1-gpio,gpiopin=4
+```
+
+After rebooting the files should show up.
+"""
 import glob
+import os
 import time
 
 os.system('modprobe w1-gpio')
@@ -8,14 +22,18 @@ base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 
+
 def read_temp_raw():
-	f = open(device_file, 'r')
-	lines = f.readlines()
-	f.close()
-	return lines
+    """Read the raw temperature from the device."""
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
 
 def read_temp():
-	lines = read_temp_raw()
+    """Read the temperature of the probe."""
+    lines = read_temp_raw()
     while lines[0].strip()[-3:] != 'YES':
         time.sleep(0.2)
         lines = read_temp_raw()
@@ -25,6 +43,7 @@ def read_temp():
             temp_c = float(temp_string) / 1000.0
             temp_f = temp_c * 9.0 / 5.0 + 32.0
             return temp_c, temp_f
+
 
 while True:
         print(read_temp())
